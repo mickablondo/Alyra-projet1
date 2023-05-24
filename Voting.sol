03c3a66bea52d441ce2504e5d9e497b481bb6e66
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-05-24 23:36:30 +0200

    Modif licence

--- Voting.sol
+++ Voting.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
